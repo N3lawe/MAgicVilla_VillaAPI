bbed6574460e75422750cbd418d09b767bee2e8a
Laith Subbh <Lsubbh@kayanhr.com>
2025-04-07 15:16:08 +0300

    add new API

---
+++ MAgicVilla_VillaAPI/Controllers/VillaAPIController.cs
@@ -0,0 +1,35 @@
+﻿using MAgicVilla_VillaAPI.Data;
+using MAgicVilla_VillaAPI.Models;
+using MAgicVilla_VillaAPI.Models.Dto;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MAgicVilla_VillaAPI.Controllers
+{
+    //[Route("api/[controller]")]
+    [Route("api/VillaAPI")] // name of rout api
+
+    [ApiController]
+    public class VillaAPIController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<IEnumerable<VillaDTO>> GetVillas()
+        {
+            return Ok(VillaStore.villaList);
+        }
+
+        [HttpGet("{id:int}")]
+        public ActionResult<VillaDTO> GetVilla(int id)
+        {
+            if (id == 0)
+            {
+                return BadRequest();
+            }
+            var villa = VillaStore.villaList.FirstOrDefault(u => u.Id == id);
+            if (villa == null)
+            {
+                return NotFound();
+            }
+            return Ok(villa);
+        }
+    }
+}
---
+++ MAgicVilla_VillaAPI/Data/VillaStore.cs
@@ -0,0 +1,13 @@
+﻿using MAgicVilla_VillaAPI.Models.Dto;
+
+namespace MAgicVilla_VillaAPI.Data
+{
+    public static class VillaStore
+    {
+        public static List<VillaDTO> villaList = new List<VillaDTO>
+            {
+                new VillaDTO{Id = 1, Name ="Pool View"},
+                new VillaDTO{Id = 2, Name ="Beach View"}
+            };
+    }
+}

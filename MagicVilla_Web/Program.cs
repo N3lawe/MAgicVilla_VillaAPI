801489645a3ae0ca71f217c40f7356598fab3509
mohannadsroor18 <mohannadsroor18@hotmail.com>
2025-06-18 18:58:18 +0300

    add VillaController

--- MagicVilla_Web/Program.cs
+++ MagicVilla_Web/Program.cs
@@ -1,6 +1,7 @@
 using MagicVilla_Web.Service;
 using MagicVilla_Web.Service.IService;
 using MAgicVilla_Web;
+using Microsoft.AspNetCore.Authentication.Cookies;
 
 namespace MagicVilla_Web
 {
@@ -9,14 +10,35 @@ namespace MagicVilla_Web
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+
             builder.Services.AddControllersWithViews();
             builder.Services.AddAutoMapper(typeof(MappingConfig));
+
 
             builder.Services.AddHttpClient<IVillaService, VillaService>();
             builder.Services.AddScoped<IVillaService, VillaService>();
 
-
-
+            //builder.Services.AddHttpClient<IVillaNumberService, VillaNumberService>();
+            //builder.Services.AddScoped<IVillaNumberService, VillaNumberService>();
+            builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+            //builder.Services.AddHttpClient<IAuthService, AuthService>();
+            //builder.Services.AddScoped<IAuthService, AuthService>();
+            builder.Services.AddDistributedMemoryCache();
+            builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+                          .AddCookie(options =>
+                          {
+                              options.Cookie.HttpOnly = true;
+                              options.ExpireTimeSpan = TimeSpan.FromMinutes(30);
+                              options.LoginPath = "/Auth/Login";
+                              options.AccessDeniedPath = "/Auth/AccessDenied";
+                              options.SlidingExpiration = true;
+                          });
+            builder.Services.AddSession(options =>
+            {
+                options.IdleTimeout = TimeSpan.FromMinutes(100);
+                options.Cookie.HttpOnly = true;
+                options.Cookie.IsEssential = true;
+            });
             var app = builder.Build();
 
             if (!app.Environment.IsDevelopment())
@@ -26,15 +48,16 @@ namespace MagicVilla_Web
             }
 
             app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
             app.UseRouting();
-
+            app.UseAuthentication();
             app.UseAuthorization();
-
-            app.MapStaticAssets();
+            app.UseSession();
             app.MapControllerRoute(
                 name: "default",
-                pattern: "{controller=Home}/{action=Index}/{id?}")
-                .WithStaticAssets();
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+
             app.Run();
         }
     }

ff045e7256b503f041e1cc2be6e03b84ea458c61
mohannadsroor <mohannadsroor18@hotmail.com>
2025-06-18 16:21:35 +0300

    add MappingConfig

--- MagicVilla_Web/Program.cs
+++ MagicVilla_Web/Program.cs
@@ -1,3 +1,5 @@
+using MAgicVilla_Web;
+
 namespace MagicVilla_Web
 {
     public class Program
@@ -5,17 +7,13 @@ namespace MagicVilla_Web
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
-
-            // Add services to the container.
             builder.Services.AddControllersWithViews();
-
+            builder.Services.AddAutoMapper(typeof(MappingConfig));
             var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
             if (!app.Environment.IsDevelopment())
             {
                 app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
 
@@ -29,7 +27,6 @@ namespace MagicVilla_Web
                 name: "default",
                 pattern: "{controller=Home}/{action=Index}/{id?}")
                 .WithStaticAssets();
-
             app.Run();
         }
     }
